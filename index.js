52479c109cfcc021b2ad7cb9ea68d74e4748cb4d
Jagadeesh <74336885+0-jagadeesh-0@users.noreply.github.com>
2022-12-26 18:06:30 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    var typed = new Typed(".auto-text",
+        {
+            strings: ["Jagadeesh", "a Full Stack Web Developer"],
+            typeSpeed: 100,
+            backSpeed: 100,
+            loop: true
+        })
+    var about = new Typed(".about-text",
+        {
+            strings: ["I am Currently Pursuing my final year Btech in Electronics and Communication Engineering at NIT Agartala.",
+                "I am a Full Stack Web Developer.",
+                "I am interested in learning new technologies.",
+                "I am good problem solver."],
+            typeSpeed: 100,
+            backSpeed: 10,
+            loop: true
+        })
+})
+
